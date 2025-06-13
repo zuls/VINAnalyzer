b6617394c60fbb6529813de129a9222bc9fa4127
zuls <zularbine@gmail.com>
2025-06-13 15:23:54 -0400

    Initial Commit

---
+++ VINAnalyzer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VINAnalyzer
+//
+//  Created by Zularbine Kamal on 6/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VINAnalyzer/VINAnalyzerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VINAnalyzerApp.swift
+//  VINAnalyzer
+//
+//  Created by Zularbine Kamal on 6/13/25.
+//
+
+import SwiftUI
+
+@main
+struct VINAnalyzerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
